6c8c8bcc1b1bb6dccd32fd8f67817fe21ed9734e
adil Mehmood <adil@appliedblockchain.com>
2022-03-22 10:40:19 +0000

    AVP-37: Avp-37-FE-Unit_Tests Text FIeld Test has been implemented for example

--- packages/ui-library/src/TextField/TextField.tsx
+++ packages/ui-library/src/TextField/TextField.tsx
@@ -5,14 +5,14 @@ interface Props {
   label: string
   error?: string
 }
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & Props
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & Props | any
 
 const TextField = React.forwardRef<HTMLInputElement, InputProps>(
   (props: InputProps, ref) => {
     const { label, error, ...inputProps } = props
     return (
       <div className="form-control">
-        <input ref={ref} placeholder={label} {...inputProps} />
+        <input ref={ref} data-testid="input" placeholder={label} {...inputProps} />
         {error && <span> {error}</span>}
       </div>
     )
