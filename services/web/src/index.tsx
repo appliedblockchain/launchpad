2dec84d6b1c6862fd05d83675e515b84bda52ea4
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2023-09-08 16:48:11 +0300

    upgrade react rendering via createRoot

--- services/web/src/index.tsx
+++ services/web/src/index.tsx
@@ -5,14 +5,17 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
