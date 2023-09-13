2bcb0be4eb49318b278ed70cda189cb738ccb3b0
Samuel Coelho <samuelfcmc@gmail.com>
2023-09-13 16:04:27 +0100

    Fixes web tests

--- services/web/src/App.test.tsx
+++ services/web/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import App from './App'
+import { renderWithProviders } from './test/utils'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  renderWithProviders(<App />)
+  const linkElements = screen.getAllByText(/login/i)
+  expect(linkElements.length).toBeGreaterThan(0)
 })
